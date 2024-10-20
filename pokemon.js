526456c9cfabe6bbcfd3909b14860b3bfc6bacb9
Samuel Plaza <samuelplazasaez@gmail.com>
2024-10-21 00:18:03 +0200

    juego modificado con fetch, para poder añadir todos los pokémoms que queramos

--- pokemon.js
+++ pokemon.js
@@ -1,205 +1,293 @@
-const pokemonsP1 = [
-	{
-		nombre: 'Pikachu',
-		tipo: 'Eléctrico',
-		fuerza: 50,
-		img: './images/pikachu.png',
-		vida: 100,
+let datos
+
+const options = {
+	method: 'GET',
+	headers: {
+		'User-Agent': 'insomnia/10.1.0',
+		apikey:
+			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqb2xweGJrb3B3bG16enRwbWdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkwNzEwMDksImV4cCI6MjA0NDY0NzAwOX0.IeR7NSHpXXJwTa0D84ov2dQ8BJgHAjxwyQPLtj4LfKg',
 	},
-	{
-		nombre: 'Charizard',
-		tipo: 'Fuego/Volador',
-		fuerza: 80,
-		img: './images/charizard.png',
-		vida: 120,
-	},
-	{
-		nombre: 'Bulbasur',
-		tipo: 'Planta/Veneno',
-		fuerza: 60,
-		img: './images/bulbasur.png',
-		vida: 90,
-	},
-]
+}
+fetch(
+	'https://bjolpxbkopwlmzztpmgb.supabase.co/rest/v1/pokemon?select=*',
+	options,
+)
+	.then(response => response.json())
+	.then(data => {
+		const datos = visualizar(data)
+		console.log(datos)
+		const botonLucha = document.querySelector('.boton-luchar')
+		botonLucha.addEventListener('click', () => ataque(datos))
+	})
+	.catch(err => console.error(err))
+
+function visualizar(data) {
+	const pokemonsP1 = data.filter(e => e.UserId === 1)
+	let copiaP1 = pokemonsP1.map(e => ({ ...e, vidaActual: e.vida }))
+
+	const pokemonsP2 = data.filter(e => e.UserId === 2)
+	let copiaP2 = pokemonsP2.map(e => ({ ...e, vidaActual: e.vida }))
+
+	copiaP1.forEach(e => {
+		let imagenes = document.querySelector('#imagesP1')
+		let divPokemon = document.createElement('div')
+		divPokemon.setAttribute('id', `P1-${e.nombre}`)
+		divPokemon.classList.add('P-pokemons')
+		let imgPokemon = document.createElement('img')
+		imgPokemon.setAttribute('id', `imgP1-${e.nombre}`)
+		imgPokemon.setAttribute('src', e.img)
+		let vidaPok = document.createElement('p')
+		vidaPok.setAttribute('id', `vidaP1-${e.nombre}`)
+		vidaPok.innerText = e.vidaActual + '/' + e.vida
+		divPokemon.appendChild(imgPokemon)
+		divPokemon.appendChild(vidaPok)
+		imagenes.appendChild(divPokemon)
+
+		divPokemon.addEventListener('click', () => {
+			if (e.vidaActual > 0) {
+				document.querySelector('.boton-luchar').removeAttribute('disabled')
+				document.querySelector('#imgP1').classList.remove('muerto')
+				copiaP1.forEach(pok => (pok.enUso = false))
+				e.enUso = true
+			}
+			if (e.vidaActual === 0) {
+				document.querySelector('.boton-luchar').setAttribute('disabled', '')
+				document.querySelector('#imgP1').classList.add('muerto')
+			}
+
+			const elegir = document.getElementById('span1')
+			elegir.classList.add('oculto')
+			const imgPok1Batalla = document.getElementById(`imgP1`)
+			imgPok1Batalla.setAttribute('src', e.img)
+			const nombrePok1Batalla = document.getElementById('nombrePok1Batalla')
+			nombrePok1Batalla.classList.remove('oculto')
+			nombrePok1Batalla.innerText = e.nombre
+			const vidaPok1Batalla = document.getElementById('vidaPok1Batalla')
+			vidaPok1Batalla.classList.remove('oculto')
+			vidaPok1Batalla.innerText = `Vida: ${e.vidaActual}`
+
+			const tipoPok1Batalla = document.getElementById('tipoPok1Batalla')
+			tipoPok1Batalla.classList.remove('oculto')
+			tipoPok1Batalla.innerText = `Tipo: ${e.tipo}`
+
+			const fuerzaPok1Batalla = document.getElementById('fuerzaPok1Batalla')
+			fuerzaPok1Batalla.classList.remove('oculto')
+			fuerzaPok1Batalla.innerText = `Fuerza: ${e.fuerza}`
+		})
+	})
+
+	copiaP2.forEach(e => {
+		let imagenes2 = document.querySelector('#imagesP2')
+		let divPokemon2 = document.createElement('div')
+		divPokemon2.setAttribute('id', `P2-${e.nombre}`)
+		divPokemon2.classList.add('P-pokemons')
+		let imgPokemon2 = document.createElement('img')
+		imgPokemon2.setAttribute('id', `imgP2-${e.nombre}`)
+		imgPokemon2.setAttribute('src', e.img)
+		let vidaPok2 = document.createElement('p')
+		vidaPok2.setAttribute('id', `vidaP2-${e.nombre}`)
+		vidaPok2.innerText = e.vidaActual + '/' + e.vida
+		divPokemon2.appendChild(imgPokemon2)
+		divPokemon2.appendChild(vidaPok2)
+		imagenes2.appendChild(divPokemon2)
+
+		divPokemon2.addEventListener('click', () => {
+			if (e.vidaActual > 0) {
+				document.querySelector('.boton-luchar').removeAttribute('disabled')
+				document.querySelector('#imgP2').classList.remove('muerto')
+				copiaP2.forEach(pok => (pok.enUso = false))
+				e.enUso = true
+			}
+			if (e.vidaActual === 0) {
+				document.querySelector('.boton-luchar').setAttribute('disabled', '')
+				document.querySelector('#imgP2').classList.add('muerto')
+			}
+
+			const elegir = document.getElementById('span2')
+			elegir.classList.add('oculto')
+			const imgPok2Batalla = document.getElementById(`imgP2`)
+			imgPok2Batalla.setAttribute('src', e.img)
+			const nombrePok2Batalla = document.getElementById('nombrePok2Batalla')
+			nombrePok2Batalla.classList.remove('oculto')
+			nombrePok2Batalla.innerText = e.nombre
+			const vidaPok2Batalla = document.getElementById('vidaPok2Batalla')
+			vidaPok2Batalla.classList.remove('oculto')
+			vidaPok2Batalla.innerText = `Vida: ${e.vidaActual}`
+
+			const tipoPok2Batalla = document.getElementById('tipoPok2Batalla')
+			tipoPok2Batalla.classList.remove('oculto')
+			tipoPok2Batalla.innerText = `Tipo: ${e.tipo}`
+
+			const fuerzaPok2Batalla = document.getElementById('fuerzaPok2Batalla')
+			fuerzaPok2Batalla.classList.remove('oculto')
+			fuerzaPok2Batalla.innerText = `Fuerza: ${e.fuerza}`
+		})
+	})
+	return { copiaP1, copiaP2 }
+}
 
 let pokVivosP1 = 3
-
-const copiaP1 = pokemonsP1.map(e => ({ ...e, vidaActual: e.vida }))
-
-const pokemonsP2 = [
-	{
-		nombre: 'Ekans',
-		tipo: 'Veneno',
-		fuerza: 50,
-		img: './images/ekans.png',
-		vida: 130,
-	},
-	{
-		nombre: 'Lapras',
-		tipo: 'Agua',
-		fuerza: 70,
-		img: './images/lapras.png',
-		vida: 100,
-	},
-	{
-		nombre: 'Snorlax',
-		tipo: 'Normal',
-		fuerza: 100,
-		img: './images/snorlax.png',
-		vida: 150,
-	},
-]
-
 let pokVivosP2 = 3
 
-const copiaP2 = pokemonsP2.map(e => ({ ...e, vidaActual: e.vida }))
+let turno = 1
 
-copiaP1.forEach((e, i) => {
-	let imgPokemon = document.getElementById(`imgP1-${i}`)
-	imgPokemon.setAttribute('src', e.img)
-	let vidaPok = document.getElementById(`vidaP1 ${i}`)
-	vidaPok.innerText = e.vidaActual + '/' + e.vida
+function ataque(datos) {
+	let pokBatallaP1 = datos.copiaP1.filter(e => e.enUso === true)
+	let pok1Batalla = pokBatallaP1[0]
+	let pokBatallaP2 = datos.copiaP2.filter(e => e.enUso === true)
+	let pok2Batalla = pokBatallaP2[0]
 
-	const P1 = document.getElementById(`P1-${i}`)
-	P1.addEventListener('click', () => {
-		if (e.vidaActual > 0) {
-			document.querySelector('.boton-luchar').removeAttribute('disabled')
-			document.querySelector('#imgP1').classList.remove('muerto')
-		}
-		if (e.vidaActual === 0) {
+	if (pok1Batalla.vidaActual === 0 || pok2Batalla.vidaActual === 0) {
+		document.querySelector('.boton-luchar').setAttribute('disabled', '')
+		return
+	}
+
+	// Ataque del Pokémon 1
+	if (turno % 2 !== 0) {
+		let ataque1 = Math.ceil(Math.random() * pok1Batalla.fuerza)
+		pok2Batalla.vidaActual -= ataque1
+
+		if (pok2Batalla.vidaActual <= 0) {
+			pok2Batalla.vidaActual = 0
 			document.querySelector('.boton-luchar').setAttribute('disabled', '')
-			document.querySelector('#imgP1').classList.add('muerto')
+			document
+				.querySelector(`#P2-${pok2Batalla.nombre}`)
+				.classList.add('muerto')
+			document.querySelector(`#imgP2`).classList.add('muerto')
+			pokVivosP2 -= 1
+			if (pokVivosP2 === 0) {
+				let ganador = document.createElement('p')
+				ganador.className = 'ganador'
+				ganador.innerHTML = '¡Player 1 win!'
+				document.body.append(ganador)
+			}
 		}
-		const elegir = document.getElementById('span1')
-		elegir.classList.add('oculto')
-		const imgPok1Batalla = document.getElementById(`imgP1`)
-		imgPok1Batalla.setAttribute('src', e.img)
-		const nombrePok1Batalla = document.getElementById('nombrePok1Batalla')
-		nombrePok1Batalla.classList.remove('oculto')
-		nombrePok1Batalla.innerText = e.nombre
-		const vidaPok1Batalla = document.getElementById('vidaPok1Batalla')
-		vidaPok1Batalla.classList.remove('oculto')
-		vidaPok1Batalla.innerText = `Vida: ${e.vidaActual}`
 
-		const tipoPok1Batalla = document.getElementById('tipoPok1Batalla')
-		tipoPok1Batalla.classList.remove('oculto')
-		tipoPok1Batalla.innerText = `Tipo: ${e.tipo}`
+		// Actualizar el texto de vida del Pokémon 2
+		let vidaPok2Batalla = document.getElementById('vidaPok2Batalla')
+		vidaPok2Batalla.innerText = `Vida: ${pok2Batalla.vidaActual}`
 
-		const fuerzaPok1Batalla = document.getElementById('fuerzaPok1Batalla')
-		fuerzaPok1Batalla.classList.remove('oculto')
-		fuerzaPok1Batalla.innerText = `Fuerza: ${e.fuerza}`
-	})
-})
+		let vidaPok2 = document.querySelector(`#vidaP2-${pok2Batalla.nombre}`)
+		vidaPok2.innerText = pok2Batalla.vidaActual + '/' + pok2Batalla.vida
 
-copiaP2.forEach((e, i) => {
-	let imgPokemon = document.getElementById(`imgP2-${i}`)
-	imgPokemon.setAttribute('src', e.img)
-	let vidaPok = document.getElementById(`vidaP2 ${i}`)
-	vidaPok.innerText = e.vidaActual + '/' + e.vida
+		const ataquePok1Batalla = document.getElementById('eventos-batalla')
+		ataquePok1Batalla.innerHTML += `Ataca ${pok1Batalla.nombre} con daño ${ataque1} y deja a ${pok2Batalla.nombre} con ${pok2Batalla.vidaActual} de vida.<br>`
+		ataquePok1Batalla.scrollTop = ataquePok1Batalla.scrollHeight
 
-	const P2 = document.getElementById(`P2-${i}`)
-	P2.addEventListener('click', () => {
-		if (e.vidaActual > 0) {
-			document.querySelector('.boton-luchar').removeAttribute('disabled')
-			document.querySelector('#imgP2').classList.remove('muerto')
-		}
-		if (e.vidaActual === 0) {
+		if (pok2Batalla.vidaActual === 0) {
 			document.querySelector('.boton-luchar').setAttribute('disabled', '')
-			document.querySelector('#imgP2').classList.add('muerto')
+			turno++
+			return
 		}
-		const elegir = document.getElementById('span2')
-		elegir.classList.add('oculto')
-		const imgPok2Batalla = document.getElementById(`imgP2`)
-		imgPok2Batalla.setAttribute('src', e.img)
-		const nombrePok2Batalla = document.getElementById('nombrePok2Batalla')
-		nombrePok2Batalla.classList.remove('oculto')
-		nombrePok2Batalla.innerText = e.nombre
-		const vidaPok2Batalla = document.getElementById('vidaPok2Batalla')
-		vidaPok2Batalla.classList.remove('oculto')
-		vidaPok2Batalla.innerText = `Vida: ${e.vidaActual}`
+	} else if (turno % 2 === 0) {
+		// Ataque del Pokémon 2
+		let ataque2 = Math.ceil(Math.random() * pok2Batalla.fuerza)
+		pok1Batalla.vidaActual -= ataque2
 
-		const tipoPok2Batalla = document.getElementById('tipoPok2Batalla')
-		tipoPok2Batalla.classList.remove('oculto')
-		tipoPok2Batalla.innerText = `Tipo: ${e.tipo}`
-
-		const fuerzaPok2Batalla = document.getElementById('fuerzaPok2Batalla')
-		fuerzaPok2Batalla.classList.remove('oculto')
-		fuerzaPok2Batalla.innerText = `Fuerza: ${e.fuerza}`
-	})
-})
-
-const ataque = () => {
-	const nombrePok1Batalla = document.getElementById('nombrePok1Batalla')
-	pok1Batalla = copiaP1.filter(e => e.nombre === nombrePok1Batalla.innerHTML)
-
-	const nombrePok2Batalla = document.getElementById('nombrePok2Batalla')
-	pok2Batalla = copiaP2.filter(e => e.nombre === nombrePok2Batalla.innerHTML)
-
-	copiaP1.map((e1, i1) => {
-		if (e1.nombre === pok1Batalla[0].nombre) {
-			copiaP2.map((e2, i2) => {
-				if (e2.nombre === pok2Batalla[0].nombre) {
-					if (e1.vidaActual === 0 || e2.vidaActual === 0) {
-						document.querySelector('.boton-luchar').setAttribute('disabled', '')
-						return
-					}
-					let ataque1 = Math.ceil(Math.random() * e1.fuerza)
-					e2.vidaActual -= ataque1
-					if (e2.vidaActual <= 0) {
-						e2.vidaActual = 0
-						document.querySelector('.boton-luchar').setAttribute('disabled', '')
-						document.querySelector(`#P2-${i2}`).classList.add('muerto')
-						document.querySelector(`#imgP2`).classList.add('muerto')
-						pokVivosP2 -= 1
-						if (pokVivosP2 === 0) {
-							let ganador = document.createElement('p')
-							ganador.className = 'ganador'
-							ganador.innerHTML = '¡Player 1 win!'
-							document.body.append(ganador)
-						}
-					}
-					let vidaPok2 = document.getElementById(`vidaP2 ${i2}`)
-					vidaPok2.innerText = e2.vidaActual + '/' + e2.vida
-					let vidaPok2Batalla = document.getElementById('vidaPok2Batalla')
-					vidaPok2Batalla.innerText = `Vida: ${e2.vidaActual}`
-					const ataquePok1Batalla = document.getElementById('eventos-batalla')
-					ataquePok1Batalla.innerHTML += `Ataca ${e1.nombre} con daño ${ataque1} y deja a ${e2.nombre} con ${e2.vidaActual} de vida.<br>`
-					ataquePok1Batalla.scrollTop = ataquePok1Batalla.scrollHeight
-					if (e2.vidaActual === 0) {
-						document.querySelector('.boton-luchar').setAttribute('disabled', '')
-						return
-					}
-					let ataque2 = Math.ceil(Math.random() * e2.fuerza)
-					e1.vidaActual -= ataque2
-					if (e1.vidaActual <= 0) {
-						e1.vidaActual = 0
-						document.querySelector('.boton-luchar').setAttribute('disabled', '')
-						document.querySelector(`#P1-${i1}`).classList.add('muerto')
-						document.querySelector(`#imgP1`).classList.add('muerto')
-						pokVivosP1 -= 1
-						if (pokVivosP1 === 0) {
-							let ganador = document.createElement('p')
-							ganador.className = 'ganador'
-							ganador.innerHTML = '¡Player 2 win!'
-							document.body.append(ganador)
-						}
-					}
-					let vidaPok1 = document.getElementById(`vidaP1 ${i1}`)
-					vidaPok1.innerText = e1.vidaActual + '/' + e1.vida
-					let vidaPok1Batalla = document.getElementById('vidaPok1Batalla')
-					vidaPok1Batalla.innerText = `Vida: ${e1.vidaActual}`
-					const ataquePok2Batalla = document.getElementById('eventos-batalla')
-					ataquePok2Batalla.innerHTML += `Ataca ${e2.nombre} con daño ${ataque2} y deja a ${e1.nombre} con ${e1.vidaActual} de vida.<br>`
-					ataquePok2Batalla.scrollTop = ataquePok2Batalla.scrollHeight
-					if (e1.vidaActual === 0) {
-						document.querySelector('.boton-luchar').setAttribute('disabled', '')
-						return
-					}
-				}
-			})
+		if (pok1Batalla.vidaActual <= 0) {
+			pok1Batalla.vidaActual = 0
+			document.querySelector('.boton-luchar').setAttribute('disabled', '')
+			document
+				.querySelector(`#P1-${pok1Batalla.nombre}`)
+				.classList.add('muerto')
+			document.querySelector(`#imgP1`).classList.add('muerto')
+			pokVivosP1 -= 1
+			if (pokVivosP1 === 0) {
+				let ganador = document.createElement('p')
+				ganador.className = 'ganador'
+				ganador.innerHTML = '¡Player 2 win!'
+				document.body.append(ganador)
+			}
 		}
-	})
+
+		// Actualizar el texto de vida del Pokémon 1
+		let vidaPok1Batalla = document.getElementById('vidaPok1Batalla')
+		vidaPok1Batalla.innerText = `Vida: ${pok1Batalla.vidaActual}`
+
+		let vidaPok1 = document.querySelector(`#vidaP1-${pok1Batalla.nombre}`)
+		vidaPok1.innerText = pok1Batalla.vidaActual + '/' + pok1Batalla.vida
+
+		const ataquePok2Batalla = document.getElementById('eventos-batalla')
+		ataquePok2Batalla.innerHTML += `Ataca ${pok2Batalla.nombre} con daño ${ataque2} y deja a ${pok1Batalla.nombre} con ${pok1Batalla.vidaActual} de vida.<br>`
+		ataquePok2Batalla.scrollTop = ataquePok2Batalla.scrollHeight
+
+		if (pok1Batalla.vidaActual === 0) {
+			document.querySelector('.boton-luchar').setAttribute('disabled', '')
+			turno++
+			return
+		}
+	}
+
+	console.log('Turno:', turno)
+	turno++
 }
+
+// function ataque(datos) {
+// 	pokBatallaP1 = datos.copiaP1.filter(e => e.enUso === true)
+// 	pok1Batalla = pokBatallaP1[0]
+
+// 	pokBatallaP2 = datos.copiaP2.filter(e => e.enUso === true)
+// 	pok2Batalla = pokBatallaP2[0]
+
+// 	if (pok1Batalla.vidaActual === 0 || pok2Batalla.vidaActual === 0) {
+// 		document.querySelector('.boton-luchar').setAttribute('disabled', '')
+// 		return
+// 	}
+// 	let ataque1 = Math.ceil(Math.random() * pok1Batalla.fuerza)
+// 	pok2Batalla.vidaActual -= ataque1
+// 	if (pok2Batalla.vidaActual <= 0) {
+// 		pok2Batalla.vidaActual = 0
+// 		document.querySelector('.boton-luchar').setAttribute('disabled', '')
+// 		document.querySelector(`#P2-${pok2Batalla.nombre}`).classList.add('muerto')
+// 		document.querySelector(`#imgP2`).classList.add('muerto')
+// 		pokVivosP2 -= 1
+// 		if (pokVivosP2 === 0) {
+// 			let ganador = document.createElement('p')
+// 			ganador.className = 'ganador'
+// 			ganador.innerHTML = '¡Player 1 win!'
+// 			document.body.append(ganador)
+// 		}
+// 	}
+// 	let vidaPok2 = pok2Batalla.vidaActual
+// 	vidaPok2.innerText = pok2Batalla.vidaActual + '/' + pok2Batalla.vida
+// 	let vidaPok2Batalla = document.getElementById('vidaPok2Batalla')
+// 	vidaPok2Batalla.innerText = `Vida: ${pok2Batalla.vidaActual}`
+// 	const ataquePok1Batalla = document.getElementById('eventos-batalla')
+// 	ataquePok1Batalla.innerHTML += `Ataca ${pok1Batalla.nombre} con daño ${ataque1} y deja a ${pok2Batalla.nombre} con ${pok2Batalla.vidaActual} de vida.<br>`
+// 	ataquePok1Batalla.scrollTop = ataquePok1Batalla.scrollHeight
+// 	if (pok2Batalla.vidaActual === 0) {
+// 		document.querySelector('.boton-luchar').setAttribute('disabled', '')
+// 		return
+// 	}
+// 	let ataque2 = Math.ceil(Math.random() * pok2Batalla.fuerza)
+// 	pok1Batalla.vidaActual -= ataque2
+// 	if (pok1Batalla.vidaActual <= 0) {
+// 		pok1Batalla.vidaActual = 0
+// 		document.querySelector('.boton-luchar').setAttribute('disabled', '')
+// 		document.querySelector(`#P1-${pok1Batalla.nombre}`).classList.add('muerto')
+// 		document.querySelector(`#imgP1`).classList.add('muerto')
+// 		pokVivosP1 -= 1
+// 		if (pokVivosP1 === 0) {
+// 			let ganador = document.createElement('p')
+// 			ganador.className = 'ganador'
+// 			ganador.innerHTML = '¡Player 2 win!'
+// 			document.body.append(ganador)
+// 		}
+// 	}
+// 	let vidaPok1 = pok1Batalla.vidaActual
+// 	vidaPok1.innerText = pok1Batalla.vidaActual + '/' + pok1Batalla.vida
+// 	let vidaPok1Batalla = document.getElementById('vidaPok1Batalla')
+// 	vidaPok1Batalla.innerText = `Vida: ${pok1Batalla.vidaActual}`
+// 	const ataquePok2Batalla = document.getElementById('eventos-batalla')
+// 	ataquePok2Batalla.innerHTML += `Ataca ${pok2Batalla.nombre} con daño ${ataque2} y deja a ${pok1Batalla.nombre} con ${pok1Batalla.vidaActual} de vida.<br>`
+// 	ataquePok2Batalla.scrollTop = ataquePok2Batalla.scrollHeight
+// 	if (pok1Batalla.vidaActual === 0) {
+// 		document.querySelector('.boton-luchar').setAttribute('disabled', '')
+// 		return
+// 	}
+// 	turno++
+// 	console.log('Turno:', turno)
+// }
 
 const reiniciar = () => {
 	// const eventos = document.getElementById('eventos-batalla')
